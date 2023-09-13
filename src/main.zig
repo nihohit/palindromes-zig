344d62f65f8d89171019c878c8ec498f5d482579
Shachar Langbeheim <nihohit@gmail.com>
2023-09-13 21:33:07 +0300

    Naive check

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,39 @@
 const std = @import("std");
 
+fn check_if_slice_is_palindrome(slice: []u8) bool {
+    for (0..(slice.len + 1) / 2) |i| {
+        if (slice[i] != slice[slice.len - i-1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn check_if_palindrome(num: u64) error{NoSpaceLeft}!bool {
+    var buf = [_]u8{0} ** 30;
+    var binary_buf =  [_]u8{0} ** 65;
+    var slice = try std.fmt.bufPrint(&buf, "{d}", .{num});
+    var binary_slice = try std.fmt.bufPrint(&binary_buf, "{b}", .{num});
+    return check_if_slice_is_palindrome(slice) and check_if_slice_is_palindrome(binary_slice);
+}
+
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    var i:u64 = 0;
+    while (true) {
+        const res = try check_if_palindrome(i);
+        if (res) {
+            std.debug.print("Found {d}.\n", .{i});
+        }
+        i += 1;
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+test "simple test should pass" {
+    try std.testing.expect( try check_if_palindrome(1));
+    try std.testing.expect(try check_if_palindrome(0));
+    try std.testing.expect(try check_if_palindrome(3148955775598413));
+    try std.testing.expect(!try check_if_palindrome(3148955775598414));
 }
